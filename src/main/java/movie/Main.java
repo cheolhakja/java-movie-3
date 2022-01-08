5384aa84e72a6b835a7972e4c9af5cc6d8629777
jeon-ji-yoon <rebegin15@gmail.com>
2022-01-08 17:56:26 +0900

    test: 예매 테스트

--- src/main/java/movie/Main.java
+++ src/main/java/movie/Main.java
@@ -3,7 +3,6 @@ package movie;
 public class Main {
 
     public static void main(String[] args) {
-        ReservationTest reservationTest = new ReservationTest();
-        reservationTest.run();
+
     }
 }
