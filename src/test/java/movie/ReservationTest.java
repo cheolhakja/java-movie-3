89356b6255f7255d9562801eeb0c364eaeed8edc
jeon-ji-yoon <rebegin15@gmail.com>
2022-01-08 17:57:49 +0900

    test: 예매 테스트

--- src/test/java/movie/ReservationTest.java
+++ src/test/java/movie/ReservationTest.java
@@ -1,11 +1,14 @@
 package movie;
 
+import org.junit.jupiter.api.Test;
+
 public class ReservationTest {
 
-    public void run() {
+    @Test
+    public void testReservation() {
         MovieScheduleRepository movieScheduleRepository = new MovieScheduleRepository();
         movieScheduleRepository.addSchedule(new MovieSchedule(new Movie(1L), 11));
-        movieScheduleRepository.reserve(1, 1); //수동테스트
+        movieScheduleRepository.reserve(1, 1);
         movieScheduleRepository.reserve(1, 1);
     }
 }
