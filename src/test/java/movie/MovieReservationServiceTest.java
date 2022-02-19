e29e168e4b853cbb3a83494e33474b6a5421b5e4
jeon-ji-yoon <rebegin15@gmail.com>
2022-02-19 15:29:14 +0900

    remove: 불필요한 테스트 삭제

--- src/test/java/movie/MovieReservationServiceTest.java
+++ src/test/java/movie/MovieReservationServiceTest.java
@@ -1,6 +1,5 @@
 package movie;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -9,11 +8,6 @@ public class MovieReservationServiceTest {
 
     @Test
     public void testReservation() {
-        MovieReservationServiceInterface movieReservationService = new MovieReservationServiceImpl();
 
-        movieReservationService.addSchedule(new MovieSchedule(new Movie(1L)));
-
-        assertThat(movieReservationService.reserve(1, 1)).isTrue();
-        assertThat(movieReservationService.reserve(1, 1)).isFalse();
     }
 }
