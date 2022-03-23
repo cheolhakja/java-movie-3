17df01ae4d987eb187afc816ba5ebaa3742b97f6
jeon-ji-yoon <rebegin15@gmail.com>
2022-03-23 11:49:17 +0900

    test: Seat를 불변객체로 수정

--- src/test/java/javamovie/movie/domain/SeatsTest.java
+++ src/test/java/javamovie/movie/domain/SeatsTest.java
@@ -7,19 +7,19 @@ import static org.assertj.core.api.Assertions.*;
 
 public class SeatsTest {
 
-    private Seats SeatCollection(Seat... seats) {
+    private Seats SeatCollection(ISeat... seats) {
         return new Seats(seats);
     }
 
     @Test
     public void 존재하지_않은_좌석_조회() {
         Seats seats = this.SeatCollection(
-                new Seat(1L, new RowColumnPair(1,1)),
-                new Seat(2L, new RowColumnPair(1,2)),
-                new Seat(3L, new RowColumnPair(1,3))
+                new NotReservedSeat(1L, new RowColumnPair(1,1)),
+                new NotReservedSeat(2L, new RowColumnPair(1,2)),
+                new NotReservedSeat(3L, new RowColumnPair(1,3))
         );
 
-        assertThatThrownBy(() -> seats.seatToReserve(1000,1000))
+        assertThatThrownBy(() -> seats.seatToReserve(new RowColumnPair(1000, 1000)))
                 .isInstanceOf(CannotFindSeatException.class)
                 .hasMessageContaining("좌석을 찾을 수 없습니다");
     }
@@ -27,11 +27,11 @@ public class SeatsTest {
     @Test
     public void 존재하는_좌석_조회() {
         Seats seats = this.SeatCollection(
-                new Seat(1L, new RowColumnPair(1,1)),
-                new Seat(2L, new RowColumnPair(1,2)),
-                new Seat(3L, new RowColumnPair(1,3))
+                new NotReservedSeat(1L, new RowColumnPair(1,1)),
+                new NotReservedSeat(2L, new RowColumnPair(1,2)),
+                new NotReservedSeat(3L, new RowColumnPair(1,3))
         );
 
-        assertThat(seats.seatToReserve(1,1)).isInstanceOf(Seat.class);
+        assertThat(seats.seatToReserve(new RowColumnPair(1,1))).isInstanceOf(NotReservedSeat.class);
     }
 }
