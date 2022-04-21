507e6508324015110616d5eb60585b9e77306d15
jeon-ji-yoon <rebegin15@gmail.com>
2022-04-21 15:25:12 +0900

    add lombok

--- src/main/java/javamovie/movie/domain/Movie.java
+++ src/main/java/javamovie/movie/domain/Movie.java
@@ -1,7 +1,14 @@
 package javamovie.movie.domain;
 
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
 public class Movie {
+
     private Long id;
+
+    public Movie() {
+    }
 
     public Movie(Long id) {
         this.id = id;
